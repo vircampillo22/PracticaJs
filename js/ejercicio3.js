be622e4952953240b0d8578aee92c94f0885b1ea
vir <vircampillo22@gmail.com>
2023-10-23 10:58:56 -0300

    ejercicio tres de practicas js

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,2 +1,9 @@
 //Crea una variable llamada gradosCelsius y asígnale un valor en grados Celsius. Luego, utiliza la fórmula de conversión para calcular los grados Fahrenheit y almacénalos en una variable llamada gradosFahrenheit.
 //Muestra ambos valores en la consola.
+
+let gradosCelsius= 45;
+let gradosFahrenheit= (45*9/5)+32;
+
+console.log("la transformaion a gradosFahrenheit es:"+gradosFahrenheit);
+
+
