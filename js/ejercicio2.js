3c21c68ac34f30b15e375e0bfa0d6b0596cdac9f
vir <vircampillo22@gmail.com>
2023-10-23 10:43:14 -0300

    ejercicio dos de practica js

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,3 +1,9 @@
 //Crea dos variables, nombre y apellido, y asígnales valores de tu elección.
 //Luego, crea una tercera variable llamada nombreCompleto que contenga la concatenación de nombre y apellido.
 // Muestra nombreCompleto en la consola.
+
+let nombre="virginia";
+let apellido= "campillo";
+let nombreCompleto= nombre+" "+apellido;
+
+console.log("mi nombre es:" +nombreCompleto);
